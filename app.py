cee7692bfcd6cafb3e02adb5acd9d3e4ec19ba0c
Tahani-asker <54763799+Tahani-asker@users.noreply.github.com>
2019-09-06 21:57:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,12 +18,6 @@ def predictImage():
 
 
 
-def readphoto(imgstring):
-    imgdata = base64.b64decode(imgstring)
-    filename = 'some_image.jpg'  # I assume you have a way of picking unique filenames
-    with open(filename, 'wb') as f:
-        f.write(imgdata)
-
 
 
 
